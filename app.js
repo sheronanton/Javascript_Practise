04a48e0efaf266dee44b296ce0dd4d161ba8c123
sheronanton <sheronanton@gmail.com>
2021-11-28 08:36:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-let name = prompt("Enter your name");
-console.log(name);
+let restaurant = {
+  mainCourse: ["pasta", "burger", "pizza"],
+  starters: ["barbeque", "grilled"],
+  orederfood: function ({ mainCourse_item, starters_item }) {
+    console.log(
+      `Your order is ${this.mainCourse[mainCourse_item]} and ${this.starters[starters_item]}`
+    );
+  },
+};
+let myOrder = {
+  mainCourse_item: 0,
+  starters_item: 1,
+};
+
+restaurant.orederfood(myOrder);
